405e3240441bf5d255e68a13d215f0aa47c1ed21
mmeat512 <58766047+mmeat512@users.noreply.github.com>
2025-07-17 22:28:49 +0900

    feat: Update global styles and add new SVG assets for background and logo

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,7 +2,7 @@
 /** @type {import("prettier").Config} */
 const config = {
   tabWidth: 2, // 탭 너비
-  singleQuote: true, // 작은 따옴표 사용
+  singleQuote: false, // 작은 따옴표 사용
   semi: true, // 문장 끝에 세미콜론 사용
   arrowParens: 'always', // 화살표 함수 괄호 항상 사용
   trailingComma: 'all', // 객체, 배열 등 마지막 요소 뒤에 쉼표 항상 사용
