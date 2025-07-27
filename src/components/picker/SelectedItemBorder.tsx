b287d558f9a975f5e469dd65da7ce78e1500c960
mmeat512 <58766047+mmeat512@users.noreply.github.com>
2025-07-27 09:44:00 +0900

    feat: Refactor SelectedItemBorder to use SVG for background and add customizable width and height props

--- src/components/picker/SelectedItemBorder.tsx
+++ src/components/picker/SelectedItemBorder.tsx
@@ -2,21 +2,41 @@ import React from "react";
 
 interface SelectedItemBorderProps {
   children: React.ReactNode;
+  width?: number | string;
+  height?: number | string;
 }
 
 const SelectedItemBorder: React.FC<SelectedItemBorderProps> = ({
   children,
+  width = 400,
+  height = 48,
 }) => {
   return (
-    <div
-      className="relative w-[400px] h-12 bg-no-repeat bg-center bg-contain"
-      style={{
-        backgroundImage: "url('/icons/picker.svg')",
-        backgroundSize: "100% 100%",
-      }}
-    >
+    <div className="relative">
+      {/* SVG Background with proper scaling */}
+      <svg
+        width={width}
+        height={height}
+        viewBox="0 0 436 80"
+        className="absolute inset-0"
+        preserveAspectRatio="none"
+      >
+        <image
+          href="/icons/picker.svg"
+          width="436"
+          height="80"
+          preserveAspectRatio="none"
+        />
+      </svg>
+
       {/* Content overlay */}
-      <div className="absolute inset-0 flex items-center justify-center text-black font-semibold text-[16px]">
+      <div
+        className="relative flex items-center justify-center text-black font-semibold text-[16px] z-10"
+        style={{
+          width: typeof width === "number" ? `${width}px` : width,
+          height: typeof height === "number" ? `${height}px` : height,
+        }}
+      >
         {children}
       </div>
     </div>
