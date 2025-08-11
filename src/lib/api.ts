73bc1c774072965f932f68b5a8192a64a6460b0d
STkangyh <yhkang2003@gmail.com>
2025-08-11 20:30:58 +0900

    fix: baseURL hard coding

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -15,7 +15,7 @@ function resolveBaseURL() {
   console.log("[API Client] Fallback 사용:", fallbackUrl);
   return fallbackUrl;
 }
-const baseURL = resolveBaseURL();
+const baseURL = "https://49.50.133.127:8443";
 
 export const api = axios.create({
   baseURL,
